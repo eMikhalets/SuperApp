1bb47d6ee94538a6b92aa555eef500a08c31bf88
Mikhalets Evgenii <evg.mikhalets@gmail.com>
2023-07-22 00:26:06 +0700

    fix: throw

--- core/database/src/main/java/com/emikhalets/core/database/notes/LocalNotesDataSource.kt
+++ core/database/src/main/java/com/emikhalets/core/database/notes/LocalNotesDataSource.kt
@@ -5,7 +5,6 @@ import com.emikhalets.core.database.notes.table_notes.NoteDb
 import com.emikhalets.core.database.notes.table_notes.NotesDao
 import com.emikhalets.core.database.notes.table_tasks.TaskDb
 import com.emikhalets.core.database.notes.table_tasks.TasksDao
-import java.lang.RuntimeException
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
@@ -39,7 +38,6 @@ class LocalNotesDataSource @Inject constructor(
     }
 
     suspend fun insertTask(model: TaskDb): Long {
-        throw RuntimeException("aboba")
         return tasksDao.insert(model)
     }
 
